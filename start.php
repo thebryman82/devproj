813f051ecdf1551e5211da4bd102d5c1e492d5bc
thebryman <bkcox82@yahoo.com>
2023-04-29 10:57:20 -0500

    starter page

---
+++ start.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+	<?php
+		// PHP code can go here
+	?>
+	<h1>Welcome to my website</h1>
+	<p>This is the first page of my website.</p>
+</body>
+</html>
